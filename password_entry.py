22f00cba2eedc45b0a41a06ae8de2ad45ed23e40
Mvejr <tyler.jones1@my.jcu.edu.au>
2019-08-22 16:42:44 +1000

    Add password check program

--- password_entry.py
+++ password_entry.py
@@ -1,3 +1,13 @@
 """
 Tyler
 """
+
+MIN_LENGTH = 2
+MAX_LENGTH = 12
+
+user_password = input("Enter password: ")
+while len(user_password) < MIN_LENGTH or len(user_password) > MAX_LENGTH:
+    print("Invalid Password")
+    user_password = input("Enter password: ")
+
+print("*" * len(user_password))
